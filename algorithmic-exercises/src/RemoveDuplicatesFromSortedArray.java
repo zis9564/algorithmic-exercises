5841f01784c65545fde80e22a3fa1c9779847035
zis9564 <zis9564@gmail.com>
2022-08-25 17:49:59 +0300

    new algorithms has implemented

---
+++ algorithmic-exercises/src/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    
+    public int removeDuplicates(int[] nums) {
+        int index = 1;
+        int lastUniqueIdx = 0;
+        while(index < nums.length) {
+            if(nums[index] == nums[lastUniqueIdx]) {
+                nums[lastUniqueIdx] = nums[index++];
+            } else {
+                nums[++lastUniqueIdx] = nums[index++];
+
+            }
+        }
+        return lastUniqueIdx +1;
+    }
+}
